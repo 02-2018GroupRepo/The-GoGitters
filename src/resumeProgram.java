fccf9686a2226fdacba8d6f7bb95b1aeca33a49d
syntel <syntel@PHX440G3-2815X0.syntelorg.com>
2018-02-26 09:41:05 -0700

    Resume Program first commit

---
+++ src/resumeProgram.java
@@ -0,0 +1,105 @@
+public class resumeProgram {
+        public static void main(String[] argv){
+            System.out.println();
+            System.out.println(" -- The Go Gitters!!! -- ");
+            System.out.println();
+
+            // Resume Functions
+            christianResume();
+            Noelle();
+            nadarioResume();
+            Tawni();
+            azedeh();
+            hunterResume();
+            mayur();
+        }
+
+        public static void christianResume(){
+            System.out.println("Christian Walker");
+            System.out.println("\tEducation: Bachelors in Computer Engineer from Clemson University");
+            System.out.println("\tskill: JavaScript, HTML/CSS, React, C, Python");
+            System.out.println("\tExperience: Shift Manager at Wendys");
+            System.out.println();
+        }
+
+        public static void Noelle () {
+            System.out.println("Noelle Irvin");
+            System.out.println("\tEducation: Bachelor of Science in Biological Engineering, University of Florida");
+            System.out.println("\tSkills: Microsoft Office, Visio, MATLAB, SolidWorks, Visual Basic in Excel, Arduino C/C++");
+            System.out.println("\tExperience: Process Development Engineering Intern, RTI Surgical, Feb. 2016 - Feb. 2017");
+            System.out.println();
+        }
+
+        public static void Tawni () {
+            String name = "Name: Tawni Hollers";
+            String skills = "\tSkills: Java, HTML, and CSS";
+            String education = "\tEducation: Computer Science Major at the University of North Georgia";
+            String experience = "\tExperience: Shift Leader at a Sonic Drive-In (6 years)";
+            System.out.println(name);
+            System.out.println(skills);
+            System.out.println(education);
+            System.out.println(experience);
+            System.out.println();
+        }
+
+        public static void nadarioResume(){
+            System.out.printf("Name: NaDario M. Seays %n" +
+                    "------------------------------------- %n" +
+                    "Education: American Intercontinental University %n" +
+                    "   B.A. in Information Technology - The Specialization of  Software Development and analysis %n %n" +
+                    "Skills: Java Development %n %n" +
+                    "Experience: United States Marines %n");
+            System.out.println();
+        }
+
+        public static void azedeh() {
+            // write your code here
+            System.out.println("Azadeh Hashemi");
+            String skills = "\tSkills: HTML and SQL ";
+            String education = "\tEducation: Master of project managment from Marrymont";
+            String experince = "\tExperience: Mercedes Benz HQ";
+            System.out.println(skills);
+            System.out.println(education);
+            System.out.println(experince);
+            System.out.println();
+        }
+
+        public static void hunterResume() {
+            System.out.printf("Name: Hunter McAfee %n" +
+                    "-------------------------------- %n" +
+                    "Education %n" +
+                    "- B.S. in Biology from Southern Polytechnic State University, Minor in Chemistry %n" +
+                    "- General Assembly Web Development Immersive Program %n" +
+                    "%n" +
+                    "Skills %n" +
+                    "- JavaScript, Ruby %n" +
+                    "- MongoDB, Express, React, Node %n" +
+                    "- Ruby on Rails, Postgres %n" +
+                    "%n" +
+                    "Experience %n" +
+                    "- Microbiology Lab Technician for 3 years at Wuxi Apptec %n" +
+                    "- Worked at a movie theater for 4 years during college %n");
+            System.out.println();
+
+        }
+
+        public static void mayur(){
+            System.out.println("Mayurbhai Maisuria");
+            System.out.println("\t * 1256 Valley View Rd, Dunwoody, GA 30338" + " * 678-640-6867" );
+            System.out.println("\tmickmai26@gmail.com");
+            System.out.println("\tEducation");
+            System.out.println("\tGeorgia Institute of Technology, Atlanta, GA                        Spring 2015-Summer 2017");
+            System.out.println("\tBachelor of Science in Computer Science                             Graduated - August 2017");
+            System.out.println("\tThreads: Info/Internetworks and Devices");
+            System.out.println("\tMajor GPA : 3.02/4.0");
+            System.out.println("\tSkills");
+            System.out.println("\tProgramming/Scripting Languages : Java, Python, C, Swift3, JavaScript, SQL");
+            System.out.println("\tExperience");
+            System.out.println("\tTutor and Student Assistance at Georgia State University");
+            System.out.println("\tTaught students CalculusII, Java, HTML5, and CSS3");
+            System.out.println("\tProjects");
+            System.out.println("\tImplemented Web, iOS, CPU, Info Viz System, and MULE Game application");
+            System.out.println();
+        }
+
+}
