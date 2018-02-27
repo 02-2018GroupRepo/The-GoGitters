891b12416fb8f8ed9b2695cc3f9bd2f4188c51d1
syntel <syntel@PHX440G3-28160M.syntelorg.com>
2018-02-27 10:53:21 -0500

    I messed up

--- src/resumeProgram.java
+++ src/resumeProgram.java
@@ -37,7 +37,7 @@ public class resumeProgram {
             String skills = "\tSkills: Java, HTML, and CSS";
             String education = "\tEducation: Computer Science Major at the University of North Georgia";
             String experience = "\tExperience: Shift Leader at a Sonic Drive-In (6 years)";
-            String change = "\t Testing adding code to the repo"
+            String change = "\t Testing adding code to the repo";
             System.out.println(name);
             System.out.println(skills);
             System.out.println(education);
